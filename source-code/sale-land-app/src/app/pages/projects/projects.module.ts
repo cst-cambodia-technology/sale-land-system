682df337ff6c5365fd3e27c2a5f292f3e62d13e9
PhunPhola <phphola@gmail.com>
2017-07-29 09:45:04 +0700

    commit projects

--- source-code/sale-land-app/src/app/pages/projects/projects.module.ts
+++ source-code/sale-land-app/src/app/pages/projects/projects.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Projects} from "./projects.component";
-import {AppTranslationModule} from "../../app.translation.module";
-import {NgaModule} from "../../theme/nga.module";
-import {routing} from "./projects.routing";
+import { Projects } from "./projects.component";
+import { AppTranslationModule } from "../../app.translation.module";
+import { NgaModule } from "../../theme/nga.module";
+import { routing } from "./projects.routing";
 import { ProjectModalComponent } from './project-modal/project-modal.component';
-import {NgbModalModule} from "@ng-bootstrap/ng-bootstrap";
+import { NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
       CommonModule,
       AppTranslationModule,
       NgaModule,
+      FormsModule,
       NgbModalModule,
       routing,
   ],
