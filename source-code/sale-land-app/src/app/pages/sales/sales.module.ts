293e4a488c82654deeeb0c566ca6d26659a84e5a
Khon123 <sokhon123>
2017-08-18 17:12:55 +0700

    [calculate] subtotal in invoice

--- source-code/sale-land-app/src/app/pages/sales/sales.module.ts
+++ source-code/sale-land-app/src/app/pages/sales/sales.module.ts
@@ -2,12 +2,14 @@
  * Created by Sokhon Pang on 8/16/2017.
  */
 import { NgModule }      from '@angular/core';
-import { CommonModule }  from '@angular/common';
+import {CommonModule, DatePipe}  from '@angular/common';
 import {routing} from "./sales.routing";
 import {Invoices} from "./invoice/invoices";
 import {Sales} from "./sales";
 import {FormsModule} from "@angular/forms";
 import {NgaModule} from "../../theme/nga.module";
+import {AppTranslationModule} from "../../app.translation.module";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 
 @NgModule({
@@ -15,12 +17,15 @@ import {NgaModule} from "../../theme/nga.module";
         CommonModule,
         FormsModule,
         NgaModule,
+        AppTranslationModule,
+        NgbModule,
         routing
     ],
     declarations: [
         Sales,
         Invoices,
-    ]
+    ],
+    providers: [DatePipe]
 })
 export class SalesModule {
 }
